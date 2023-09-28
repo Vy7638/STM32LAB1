c080865a4f2eefad04337098eea68b7dee69563c
LAPTOP-1IHH673K\ADMIN <tranngocvybkhcm@gmail.com>
2023-09-28 10:34:55 +0700

    update ex1

--- TEST/TEST_S/Core/Src/main.c
+++ TEST/TEST_S/Core/Src/main.c
@@ -95,11 +95,11 @@ int main(void)
   HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
   while (1)
   {
-    /* USER CODE END WHILE */
 	  //HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 	  //HAL_GPIO_TogglePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin);
 	  HAL_GPIO_TogglePin(GPIOA, GPIO_Pin_5);
 	  HAL_Delay(1000);
+	/* USER CODE END WHILE */
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
