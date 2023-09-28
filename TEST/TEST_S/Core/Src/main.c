225d53fba42979f1993128e4c1a2bd026e747c8b
LAPTOP-1IHH673K\ADMIN <tranngocvybkhcm@gmail.com>
2023-09-28 10:33:31 +0700

    update

--- TEST/TEST_S/Core/Src/main.c
+++ TEST/TEST_S/Core/Src/main.c
@@ -93,9 +93,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
 	  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 	  HAL_Delay(1000);
+	/* USER CODE END WHILE */
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
